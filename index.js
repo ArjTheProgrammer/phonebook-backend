1607c3a3ec4bdd7a7bde3ba843acd03e0a7dd9e4
Jose Neil Silagan <neilsilagan@gmail.com>
2024-11-13 13:35:55 +0800

    Do Exercises 3.2

--- index.js
+++ index.js
@@ -24,8 +24,14 @@ const persons = [
     }
 ]
 
+const requestDate = new Date()
+
 app.get("/", (request, response) => {
-    response.send('<h1>Hello World!</h1>')
+    response.send('<h1>Welcome to phonebook!</h1>')
+})
+
+app.get("/info", (request,response) => {
+    response.send(`<p>Phonebook has info for ${persons.length} people</p> <p>${requestDate}</p>`)
 })
 
 app.get("/api/persons", (request, response) => {
